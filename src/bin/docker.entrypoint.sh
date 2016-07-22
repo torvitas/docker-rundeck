5cd1d032315afb58777ab5322f297ea60658b03f
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-07-22 12:15:01 +0200

    improves start up script

--- src/bin/docker.entrypoint.sh
+++ src/bin/docker.entrypoint.sh
@@ -3,5 +3,13 @@ source /usr/local/src/rundeck/bin/docker.entrypoint.functions.sh
 
 # ensuring everything is where it should be
 initRundeck
+source /etc/rundeck/profile
 
-exec ${@}
+case ${1} in
+    rundeckd)
+        prog="rundeckd"
+        rundeckd="${JAVA_HOME:-/usr}/bin/java ${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTP_PORT}"
+        runuser -s /bin/bash -l rundeck -c "$rundeckd"
+esac
+
+bash -c "${@}"
