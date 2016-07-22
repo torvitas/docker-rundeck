48e09a69c17619433726922fe3c8501da0e2242b
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-07-22 12:16:35 +0200

    ensure exit code is correct

--- src/bin/docker.entrypoint.sh
+++ src/bin/docker.entrypoint.sh
@@ -7,9 +7,11 @@ source /etc/rundeck/profile
 
 case ${1} in
     rundeckd)
+        set -e
         prog="rundeckd"
         rundeckd="${JAVA_HOME:-/usr}/bin/java ${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTP_PORT}"
         runuser -s /bin/bash -l rundeck -c "$rundeckd"
+        exit ${?}
 esac
 
-bash -c "${@}"
+exec ${@}
